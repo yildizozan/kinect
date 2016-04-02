d57df126163d7c7c9882adf901b64464ea0af609
Ozan Yıldız <developer@yildizozan.com>
2016-04-02 13:24:31 +0300

    deneme branch için

--- Kinect/Kinect.cpp
+++ Kinect/Kinect.cpp
@@ -20,7 +20,7 @@ ozansKinect::Kinect::Kinect()
 ozansKinect::Kinect::~Kinect()
 {
 	pNuiSensor->NuiShutdown();
-	//pNuiSensor = nullptr; // neden olmuyor ?
+	//pNuiSensor = nullptr; // neden olmuyor ? df
 }
 
 HRESULT ozansKinect::Kinect::Initialize()

0004e4f648261094836a35502b6a1171037d864c
Ozan Yıldız <developer@yildizozan.com>
2016-09-02 02:27:55 +0300

    adasdas

--- Kinect/Source.cpp
+++ Kinect/Source.cpp
@@ -2,28 +2,17 @@
 #include <iostream>
 #include <Windows.h>
 
-<<<<<<< HEAD
-#include "KinectProcess.h"
-=======
 // Nui Api
 #include "NuiApi.h"
 
 // My Header
 #include "Kinect.h"
->>>>>>> refs/remotes/origin/master
 
 
 using namespace OzansKinect;
 
 int main()
 {
-<<<<<<< HEAD
-	ozansKinect::KinectProcess kinect;
-	kinect.Initialize();
-
-	// Eðer el hareketi belirtilen coordinatlara gelirse programdan çýkacak.
-	kinect.ProcessSkeleton();
-=======
 	Kinect kinect;
 	
 	HRESULT hr = kinect.Connection();
@@ -33,7 +22,6 @@ int main()
 			kinect.Process();
 	}
 
->>>>>>> refs/remotes/origin/master
 
 	return 0;
 }
